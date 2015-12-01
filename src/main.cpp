46f0681403f4f544ec50cba226bb152bbc0b8b80
RaulPPelaez <raul.perez@uam.es>
2015-12-01 12:19:46 +0100

    Error checking

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,33 @@ using namespace std;
 using namespace sf;
 
 std::string fileName;
+const char * GetGLErrorStr(GLenum err)
+{
+  switch (err)
+    {
+    case GL_NO_ERROR:          return "No error";
+    case GL_INVALID_ENUM:      return "Invalid enum";
+    case GL_INVALID_VALUE:     return "Invalid value";
+    case GL_INVALID_OPERATION: return "Invalid operation";
+    case GL_STACK_OVERFLOW:    return "Stack overflow";
+    case GL_STACK_UNDERFLOW:   return "Stack underflow";
+    case GL_OUT_OF_MEMORY:     return "Out of memory";
+    default:                   return "Unknown error";
+    }
+}
+void CheckGLError()
+{
+  while (true)
+    {
+      const GLenum err = glGetError();
+      if (GL_NO_ERROR == err)
+	break;
+
+      std::cout << "GL Error: " << GetGLErrorStr(err) << std::endl;
+    }
+}
+
+
  
 class RGLContext: public RGL{
 public:
@@ -200,7 +227,7 @@ void RGLContext::upload_step(){
   
   if(Lbox[frame] == 0.0)set_box(2*max_dist[frame]);
   else{ set_box(2*Lbox[frame]); }
-  
+  CheckGLError();
 }
 
 class App{
