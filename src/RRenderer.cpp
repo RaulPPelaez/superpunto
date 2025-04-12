67897dc6623ad77e374b07df709797a14cecdefa
Raul P. Pelaez <raulppelaez@gmail.com>
2025-04-12 08:59:53 +0200

    Change font size

--- src/RRenderer.cpp
+++ src/RRenderer.cpp
@@ -7,9 +7,9 @@ RRenderer::RRenderer(std::shared_ptr<System> sys, std::shared_ptr<RWindow> in_w,
     : sys(sys), cam(cam), gscale(gscale), box(gscale, &MVP), axis(sys, in_w),
       textRenderer(sys, in_w), w(in_w) {
   picked[0] = picked[1] = -1;
-  textRenderer.setFont(sys->getInputOptions().fontName.c_str(), 45);
-  auto resolution = w->getResolution();
   auto op = sys->getInputOptions();
+  textRenderer.setFont(op.fontName.c_str(),   int(op.target_FH / 10));
+  auto resolution = w->getResolution();
   proj =
       glm::perspective(op.fov, resolution.x / (float)resolution.y, znear, zfar);
   model = glm::mat4();
